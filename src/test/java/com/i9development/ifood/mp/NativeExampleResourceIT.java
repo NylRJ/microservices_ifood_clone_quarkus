53e67692ec5f0b36418a89b36b5148ccf92987ee
Moises RZ <loveloungeoficial@gmail.com>
2021-03-14 02:40:40 -0300

    first commit

---
+++ src/test/java/com/i9development/ifood/mp/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package com.i9development.ifood.mp;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
