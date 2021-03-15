b0430d6d2a1cf8e562be281cceef2b4801c40bc4
Moises RZ <loveloungeoficial@gmail.com>
2021-03-15 03:53:11 -0300

    Implementing test the restaurantes

--- cadastro/src/main/java/com/i9development/ifood/cadastro/RestauranteResource.java
+++ cadastro/src/main/java/com/i9development/ifood/cadastro/RestauranteResource.java
@@ -14,10 +14,15 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 @Path("/restaurantes")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@Tag(name = "restaurante")
 public class RestauranteResource {
 
 	@GET
@@ -28,8 +33,9 @@ public class RestauranteResource {
 
 	@POST
 	@Transactional
-	public void adicionar(Restaurante dto) {
+	public Response adicionar(Restaurante dto) {
 		dto.persist();
+		return Response.status(Status.CREATED).build();
 	}
 
 	@PUT
@@ -46,14 +52,92 @@ public class RestauranteResource {
 		restaurante.persist();
 	}
 
-	 	@DELETE
-	    @Path("{id}")
-	    @Transactional
-	    public void delete(@PathParam("id") Long id) {
-	        Optional<Restaurante> restauranteOp = Restaurante.findByIdOptional(id);
-	        restauranteOp.ifPresentOrElse(Restaurante::delete, () -> {
-	            throw new NotFoundException();
-	        });
-	    }
+	@DELETE
+	@Path("{id}")
+	@Transactional
+	public Response delete(@PathParam("id") Long id) {
+		Optional<Restaurante> restauranteOp = Restaurante.findByIdOptional(id);
+		restauranteOp.ifPresentOrElse(Restaurante::delete, () -> {
+			throw new NotFoundException("Não encontrado");
+		});
+		return Response.status(Status.NO_CONTENT).build();
+	}
+
+	// Pratos
+
+	@GET
+	@Path("{idRestaurante}/pratos")
+	@Tag(name = "prato")
+	public List<Restaurante> buscarPratos(@PathParam("idRestaurante") Long idRestaurante) {
+		Optional<Restaurante> restauranteOp = Restaurante.findByIdOptional(idRestaurante);
+		if (restauranteOp.isEmpty()) {
+			throw new NotFoundException("Restaurante não existe");
+		}
+
+		return Prato.list("restaurante", restauranteOp.get());
+	}
+
+	@POST
+	@Path("{idRestaurante}/pratos")
+	@Transactional
+	@Tag(name = "prato")
+	public Response adicionarPrato(@PathParam("idRestaurante") Long idRestaurante, Prato dto) {
+		Optional<Restaurante> restauranteOp = Restaurante.findByIdOptional(idRestaurante);
+		if (restauranteOp.isEmpty()) {
+			throw new NotFoundException("Restaurante não existe");
+		}
+		// Utilizando dto, recebi detached entity passed to persist:
+		Prato prato = new Prato();
+		prato.nome = dto.nome;
+		prato.descricao = dto.descricao;
+
+		prato.preco = dto.preco;
+		prato.restaurante = restauranteOp.get();
+		prato.persist();
+
+		return Response.status(Status.CREATED).build();
+	}
+	
+	@PUT
+	@Path("{idRestaurante}/pratos/{id}")
+	@Transactional
+	@Tag(name = "prato")
+	@Tag(name = "restaurante")
+	public Response atualizar(@PathParam("idRestaurante") Long idRestaurante,@PathParam("idRestaurante") Long id , Prato dto) {
+		Optional<Restaurante> restauranteOp = Restaurante.findByIdOptional(idRestaurante);
+		if (restauranteOp.isEmpty()) {
+			throw new NotFoundException("Restaurante não existe");
+		}
+		
+		Optional<Prato> pratoOp = Prato.findByIdOptional(id);
+		if (pratoOp.isEmpty()) {
+			throw new NotFoundException("Prato não existe");
+		}
+		Prato prato = pratoOp.get();
+		prato.preco = dto.preco;
+		prato.restaurante = restauranteOp.get();
+		prato.persist();
+
+		return Response.status(Status.NO_CONTENT).build();
+	}
+	
+	@DELETE
+	@Path("{idRestaurante}/pratos/{id}")
+	@Transactional
+	@Tag(name = "prato")
+	public Response delete(@PathParam("idRestaurante") Long idRestaurante,@PathParam("idRestaurante") Long id , Prato dto) {
+		Optional<Restaurante> restauranteOp = Restaurante.findByIdOptional(idRestaurante);
+		if (restauranteOp.isEmpty()) {
+			throw new NotFoundException("Restaurante não existe");
+		}
+		
+		Optional<Prato> pratoOp = Prato.findByIdOptional(id);
+	
+		pratoOp.ifPresentOrElse(Prato::delete,() ->{
+			throw new NotFoundException();
+		});
+
+		return Response.status(Status.NO_CONTENT).build();
+	}
 
 }
